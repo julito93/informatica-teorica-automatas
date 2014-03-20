6d841d3a7d7ba0fb62ad3373d11c4834889b57c2
leebeth.anacona@gmail.com <leebeth.anacona@gmail.com@fd12f245-8d76-8444-8246-73a609005342>
2014-03-20 01:29:02 +0000

    panel Lorena aa1.2

--- automatas/src/interfaz/InterfazWB.java
+++ automatas/src/interfaz/InterfazWB.java
@@ -46,7 +46,7 @@ public class InterfazWB extends JFrame {
 		
 		setTitle("Automatas");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1008, 409);
+		setBounds(100, 100, 1004, 374);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
